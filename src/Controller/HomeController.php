9257626c3f972bad46e02736c10c4fc22bbb85a2
AbdellahAilali <abdela47@laposte.net>
2018-10-11 18:47:55 +0200

    hotfix

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,10 +5,11 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
+
 class HomeController extends AbstractController
 {
     /**
-     * @Route("/home", name="home")
+     * @Route("/", name="home")
      */
     public function index()
     {
