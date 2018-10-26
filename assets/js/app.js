99716e5ba680bb7896509f6a826fae7e5093137b
AILALI Hassane <h.ailali@wakeonweb.com>
2018-10-26 23:47:53 +0200

    remove unused assets and add docker files

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,9 +1,21 @@
-import 'bootstrap';import 'bootstrap/dist/css/bootstrap.min.css';
-import $ from 'jquery';window.jQuery = $;window.$ = $;import 'animation.gsap';
-import TweenMax from 'TweenMax';
-import TimelineLite from 'TimelineLite';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import $ from 'jquery';
+import 'animation.gsap';
 
-import $ScrollMagic from 'ScrollMagic';window.ScrollMagic = $ScrollMagic;import 'owl.carousel2/dist/assets/owl.carousel.css';
+import $ScrollMagic from 'ScrollMagic';
+import 'owl.carousel2/dist/assets/owl.carousel.css';
 import 'owl.carousel2/dist/assets/owl.theme.default.css';
 import 'owl.carousel2/src/scss/_animate.scss';
-import custom from './custom.js';custom();require('gsap');require('owl.carousel2');require('./../plugins/fontawesome-free-5.0.1/css/fontawesome-all.css');require('../css/main_styles.css');require('../css/responsive.css');
+import custom from './custom.js';
+
+window.jQuery = $;
+window.$ = $;
+window.ScrollMagic = $ScrollMagic;
+custom();
+
+require('gsap');
+require('owl.carousel2');
+require('./../plugins/fontawesome-free-5.0.1/css/fontawesome-all.css');
+require('../css/main_styles.css');
+require('../css/responsive.css');
